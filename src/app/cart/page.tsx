324ef8025801519e9074236d4d2be6a333393a21
Aziz Limonu <azizlimonu@gmail.com>
2023-08-17 07:53:59 +0800

    fix

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -10,6 +10,7 @@ import { useSelector } from 'react-redux';
 
 const Page = () => {
   const { productData } = useSelector((state: StateProps) => state.next);
+
   return (
     <div className="max-w-screen-2xl mx-auto px-6 grid grid-cols-5 gap-10 py-4">
       {productData && productData.length > 0 ? (
@@ -19,30 +20,39 @@ const Page = () => {
               <p className="text-2xl font-semibold text-amazon_blue">
                 Shopping Cart
               </p>
-              <p className="text-lg font-semibold text-amazon_blue">Subtitle</p>
+
+              <p className="text-lg font-semibold text-amazon_blue">
+                Subtitle
+              </p>
             </div>
+
             <div className="pt-2 flex flex-col gap-2">
               {productData.map((item: StoreProduct) => (
                 <div key={item._id}>
                   <CartProduct item={item} />
                 </div>
               ))}
+
               <ResetCart />
             </div>
           </div>
+          
           <div className="bg-white h-64 col-span-1 p-4 rounded-lg flex items-center justify-center">
             <CartPayment />
           </div>
         </div>
       ) : (
-          <div className="bg-white h-64 col-span-5 flex flex-col items-center justify-center py-5 rounded-lg shadow-lg">
-            <h1 className="text-lg font-medium">Your cart is empty!</h1>
-            <Link href="/">
-              <button className="w-52 h-10 bg-amazon_blue text-white rounded-lg text-sm font-semibold hover:bg-amazon_yellow hover:text-black">
-                go to shopping
-              </button>
-            </Link>
-          </div>
+        <div className="bg-white h-64 col-span-5 flex flex-col items-center justify-center py-5 rounded-lg shadow-lg">
+          <h1 className="text-lg font-medium">
+            Your cart is empty!
+          </h1>
+
+          <Link href="/">
+            <button className="w-52 h-10 bg-amazon_blue text-white rounded-lg text-sm font-semibold hover:bg-amazon_yellow hover:text-black">
+              go to shopping
+            </button>
+          </Link>
+        </div>
       )}
     </div>
   )
