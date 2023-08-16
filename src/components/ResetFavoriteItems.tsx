236107e2c6d6c0116bd43097c5af6d334906bf29
Aziz Limonu <azizlimonu@gmail.com>
2023-08-17 07:52:19 +0800

    added

--- src/components/ResetFavoriteItems.tsx
+++ src/components/ResetFavoriteItems.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client";
+
+import { resetFavoriteData } from "@/redux/nextSlice";
+import { useDispatch } from "react-redux";
 
 const ResetFavoriteItems = () => {
+  const dispatch = useDispatch();
+  const handleResetCart = () => {
+    const confirmReset = window.confirm(
+      "Are you sure to reset your items from the cart?"
+    );
+    if (confirmReset) {
+      dispatch(resetFavoriteData());
+    }
+  };
   return (
-    <div>ResetFavoriteItems</div>
-  )
-}
+    <button
+      onClick={handleResetCart}
+      className="w-44 h-10 font-semibold bg-gray-200 rounded-lg hover:bg-red-600 hover:text-white duration-300"
+    >
+      reset cart
+    </button>
+  );
+};
 
-export default ResetFavoriteItems
+export default ResetFavoriteItems;
