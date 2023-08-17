fa45cfa51ae5f9ad291eccf70fe95ba1c9812ce6
Aziz Limonu <azizlimonu@gmail.com>
2023-08-17 08:04:15 +0800

    stripe ok, page ok, ready deploy

--- src/app/api/checkout.ts
+++ src/app/api/checkout.ts
@@ -6,7 +6,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
-) {
+) { 
   const { items, email } = req.body;
   const modifiedItems = items.map((item: StoreProduct) => ({
     quantity: item.quantity,
--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -9,7 +9,7 @@ const Page = () => {
   return (
     <div className="flex flex-col gap-2 items-center justify-center py-20">
       <h1 className="text-2xl text-hoverBg font-semibold">
-        Thank you for shopping in next_amazon_yt
+        Thank you for shopping
       </h1>
       <Link
         className="text-lg text-gray-500 hover:underline underline-offset-4 decoration-[1px] hover:text-blue-600 duration-300"
